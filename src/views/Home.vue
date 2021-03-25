038b52df9668d08b47ffba6e0a4b5e2d45b6ace9
Lucas van Heerikhuizen <lucasvanheerikhuizen@Lucass-MacBook-Pro.local>
2021-03-25 13:55:37 +0100

    Added and implemented canvas-free-drawing dependency

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="cfd"></div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
-
+import CanvasFreeDrawing from 'canvas-free-drawing'
 export default {
   name: 'home',
   components: {
     HelloWorld,
   },
+  data: () => ({
+    cfd: null
+  }),
+  mounted() {
+    // initialize
+    this.cfd = new CanvasFreeDrawing({
+      elementId: 'cfd',
+      width: 500,
+      height: 500,
+    });
+
+    // set properties
+    this.cfd.setLineWidth(10); // in px
+    this.cfd.setStrokeColor([0, 0, 255]); // in RGB
+
+    // listen to events
+    this.cfd.on({ event: 'redraw' }, this.redraw);
+  },
+  methods: {
+    redraw() {
+      // Do something
+    }
+  }
 };
 </script>
