dd33fc1caeba8fc40b74b2aba8363bbbb911064a
Lucas van Heerikhuizen <lucasvanheerikhuizen@Lucass-MacBook-Pro.local>
2021-03-25 14:48:45 +0100

    Added a toggle to switch between drawn mode and background

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,6 +18,14 @@
       v-for="(canvas, index) of canvases"
       :key="`canvas-${index}`"
     >
+      <b-form-checkbox
+        :id="`drawing-mode-checkbox-${index}`"
+        v-model="canvas.drawingMode"
+        :name="`drawing-mode-checkbox-${index}`"
+        @change="handleDrawingModeCheckboxChanged(index)"
+      >
+        Drawing mode
+      </b-form-checkbox>
       <div :id="`canvas-${index}`"></div>
     </div>
   </div>
@@ -26,16 +34,12 @@
 <script>
 // @ is an alias to /src
 import CanvasFreeDrawing from 'canvas-free-drawing';
-import { ButtonPlugin } from 'bootstrap-vue';
 
 export default {
   name: 'home',
   data: () => ({
     canvases: []
   }),
-  components: {
-    ButtonPlugin
-  },
   mounted() {
     this.addCanvas();
   },
@@ -43,11 +47,14 @@ export default {
     removeCanvas(index) {
       this.canvases.splice(index, 1);
     },
+    handleDrawingModeCheckboxChanged(index) {
+      this.canvases[index].canvas.toggleDrawingMode();
+    },
     async addCanvas() {
-      let canvas = null;
+      let canvas = { drawingMode: true, canvas: null };
       this.canvases.push(canvas);
       await this.$nextTick();
-      canvas = new CanvasFreeDrawing({
+      canvas.canvas = new CanvasFreeDrawing({
         elementId: `canvas-${this.canvases.length - 1}`,
         width: 500,
         height: 500,
