65832c844416558047ce3c87b61db5147172727f
Lucas van Heerikhuizen <lucasvanheerikhuizen@Lucass-MacBook-Pro.local>
2021-03-26 11:10:01 +0100

    Added management for clickable areas and updated the README.md file

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -28,7 +28,7 @@
       :key="`canvas-${canvas.id}`"
       class="mt-2"
     >
-      <h4>Canvas {{ index + 1}}</h4>
+      <h4>Frame {{ index + 1}}</h4>
       <div class="d-flex justify-content-center align-items-center mb-1">
         <b-form-input
           :id="`color-input-${canvas.id}`"
@@ -62,11 +62,78 @@
           Remove canvas
         </b-button>
       </div>
-      <canvas
-        :id="`canvas-${canvas.id}`"
-      ></canvas>
+      <div
+        class="text-align-center text-center"
+      >
+        <div
+          :style="`width: ${width}px; height: ${height}px`"
+          class="position-relative mx-auto"
+        >
+          <canvas
+            :id="`canvas-${canvas.id}`"
+            class="layer"
+            :style="`
+              width: ${width}px;
+              height: ${height}px;
+              z-index: ${canvas.drawingMode ? 1 : 0}`
+            "
+          ></canvas>
+          <svg
+            :id="`svg-${canvas.id}`"
+            class="svg layer"
+            :style="`
+              width: ${width}px;
+              height: ${height}px;
+              z-index: ${canvas.drawingMode ? 0 : 1}
+            `"
+          >
+            <rect
+              @click="handleClickedSvg(canvas, $event)"
+              class="clickable-rect"
+              x="0"
+              y="0"
+              :width="width"
+              :height="height"
+            >
+            </rect>
+            <rect
+              v-for="(rect, actionableIndex) in canvas.actionables"
+              :key="`actionable-${index}-${actionableIndex}`"
+              :id="`actionable-${index}-${actionableIndex}`"
+              class="actionable"
+              :x="rect.x"
+              :y="rect.y"
+              :width="rect.width"
+              :height="rect.height"
+            ></rect>
+          </svg>
+          <b-popover
+            v-for="(rect, actionableIndex) in canvas.actionables"
+            :key="`popover-${index}-${actionableIndex}`"
+            :target="`actionable-${index}-${actionableIndex}`"
+            triggers="click"
+            placement="top"
+          >
+            <template #title>Actionable</template>
+            <div class="d-flex">
+              <b-form-select
+                v-model="rect.goto"
+                class="mr-1 custom-select"
+                :options="computedFrames"
+              ></b-form-select>
+              <b-button
+                variant="danger"
+                size="sm"
+                @click="removeActionable(canvas, actionableIndex)"
+              >
+                Remove actionable
+              </b-button>
+            </div>
+          </b-popover>
+        </div>
+      </div>
     </div>
-  </div>
+   </div>
 </template>
 
 <script>
@@ -87,13 +154,27 @@ export default {
     selectedDimension: dimensions[0].value,
     width: dimensions[0].width,
     height: dimensions[0].height,
-    id: 0
+    id: 0,
+    popoverTarget: ''
   }),
   mounted() {
     this.addCanvas();
   },
   beforeMount() {
     this.dimensions = dimensions;
+  },
+  computed: {
+    computedFrames() {
+      const result = this.canvases.map(({ id }, index) => ({
+        text: `Frame ${index + 1}`,
+        value: id
+      }));
+
+      return [
+        { text: 'Select frame to goto', value: null },
+        ...result
+      ];
+    }
   },
   methods: {
     changeDimensions() {
@@ -112,9 +193,21 @@ export default {
       const index = this.getCanvasById(id);
       this.canvases.splice(index, 1);
     },
+    removeActionable(canvas, index) {
+      canvas.actionables.splice(index, 1);
+    },
     handleDrawingModeCheckboxChanged(id) {
       const index = this.getCanvasById(id);
       this.canvases[index].canvas.toggleDrawingMode();
+    },
+    handleClickedSvg(canvas, event) {
+      canvas.actionables.push({
+        x: event.offsetX,
+        y: event.offsetY,
+        width: 100,
+        height: 50,
+        goto: null
+      })
     },
     getCanvasById(id) {
       return this.canvases.findIndex(record => record.id === id);
@@ -131,7 +224,8 @@ export default {
         canvas: null,
         color: '#000000',
         lineWidth: 1,
-        id: this.id
+        id: this.id,
+        actionables: []
       };
       this.canvases.push(canvas);
       await this.$nextTick();
@@ -169,5 +263,23 @@ export default {
 
 .custom-color-input {
   max-width: 64px;
+}
+
+.layer {
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+.svg {
+  ::v-deep .actionable {
+    stroke: #0000ff;
+    fill: #8888ff;
+    opacity: 0.3;
+  }
+
+  ::v-deep .clickable-rect {
+    opacity: 0;
+  }
 }
 </style>
